072142ce739109acf1b2b073ca9f0c47fbdb328d
Robert <robert-github@posteo.de>
2024-12-08 01:59:16 +0100

    Add initial bounce (with errors)

--- entities/weapons/cannon.gd
+++ entities/weapons/cannon.gd
@@ -13,6 +13,9 @@ const EXPLOSION_SCENE = preload("res://effects/explosion.tscn")
 @export var burst_delay: float = 0.1
 @export var spray_count: int = 1
 @export var flight_range: float = 2
+@export var bounce_count: int = 0
+#@export var custom_modulate: Color = Color(255,255,255,255)
+
 @onready var camera = get_tree().get_first_node_in_group("camera")
 
 var reload_timer: Timer = Timer.new()
@@ -40,7 +43,9 @@ func apply_upgrade(upgrade: Upgrade):
 	spray_count = upgrade.fma("cannon_ball_spray_count", spray_count)
 	scale = Vector2.ONE * upgrade.fma("cannon_size", scale.x)
 	flight_range = upgrade.fma("cannon_ball_flight_range", flight_range)
-	
+	bounce_count = upgrade.fma("cannon_ball_bounce_count", bounce_count)
+	#custom_modulate = upgrade.fma("cannon_ball_modulate", custom_modulate)
+
 
 func _process(delta: float) -> void:
 	var target = global_position.angle_to_point(get_global_mouse_position())
@@ -62,10 +67,13 @@ func _process(delta: float) -> void:
 			cannonball.global_position = $SpawnAt.global_position
 			var velocity_direction = Vector2(cos(global_rotation), sin(global_rotation))
 			cannonball.velocity = velocity_direction * velocity + get_parent().velocity
+			cannonball.init_velocity = cannonball.velocity
 			cannonball.is_enemy = false
 			cannonball.scale = Vector2.ONE * ball_size * global_scale
 			cannonball.damage = damage
 			cannonball.seconds_flight_time = flight_range
+			cannonball.init_seconds_flight_time = cannonball.seconds_flight_time
+			cannonball.bounce_count = bounce_count
 		
 			Audio.play("cannon_shoot")
 			camera.trigger_shake(0.5 * ball_size, 0.03, 1, global_rotation)
--- entities/weapons/cannonball.gd
+++ entities/weapons/cannonball.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+const CANNONBALL_SCENE = preload("res://entities/weapons/cannonball.tscn")
 const RIPPLE_SCENE = preload("res://effects/RippleEffect.tscn")
+const GRACE_PERIOD_DURATION = .2
+const BOUNCE_FACTOR_ARRAY = [.5, 1, -.5, -1]
 
 var ripple_effect: CPUParticles2D = null
 var ripple_scale: float = 0.6
@@ -12,9 +15,19 @@ var distance_travelled = 0
 var damage = 10
 var is_enemy = true
 var seconds_flight_time = 2
+var bounce_count = 0
+
+var grace_period_active = false
+
+var init_velocity = Vector2.ZERO
+var init_seconds_flight_time = 0
+
 
 func _ready() -> void:
 	get_tree().create_timer(seconds_flight_time * 1.5).timeout.connect(despawn)
+	
+	if grace_period_active:
+		get_tree().create_timer(GRACE_PERIOD_DURATION).timeout.connect(func(): grace_period_active = false)
 	
 	ripple_effect = RIPPLE_SCENE.instantiate()
 	self.add_child(ripple_effect)
@@ -24,8 +37,10 @@ func _ready() -> void:
 	ripple_effect.one_shot = true
 	ripple_effect.emitting = false
 	
+	print(bounce_count)
+
+
 func despawn():
-	
 	var tween = create_tween()
 	tween.tween_property($Sprite2D, "self_modulate", Color(0.3, 0.3, 0.6, 0), 3) \
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
@@ -35,7 +50,7 @@ func despawn():
 	
 	ripple_effect.emitting = true
 	velocity *= 0.5
-	
+
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
@@ -43,6 +58,23 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_entity_entered(body: Node2D) -> void:
-	if body is Entity and body.is_enemy != is_enemy:
+	if not grace_period_active and body is Entity and body.is_enemy != is_enemy:
 		body.take_damage(damage)
+		
+		if bounce_count > 0:
+			var cannonball = CANNONBALL_SCENE.instantiate()
+			get_tree().root.add_child(cannonball)
+			cannonball.position = position
+			var randomizer_vec = Vector2(BOUNCE_FACTOR_ARRAY[randi() % BOUNCE_FACTOR_ARRAY.size()], BOUNCE_FACTOR_ARRAY[randi() % BOUNCE_FACTOR_ARRAY.size()])
+			cannonball.velocity = init_velocity * randomizer_vec
+			cannonball.seconds_flight_time = init_seconds_flight_time
+			cannonball.bounce_count = bounce_count - 1
+			cannonball.change_color(Color(240, 0, 0, 255))
+			cannonball.is_enemy = false
+			cannonball.grace_period_active = true
+		
 		queue_free()
+
+
+func change_color(color: Color) -> void:
+	$Sprite2D.self_modulate = color
