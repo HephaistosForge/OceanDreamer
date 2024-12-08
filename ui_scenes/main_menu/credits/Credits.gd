cab3d4a701bf4479f70e00725b234fdefdac9e2b
Robert <robert-github@posteo.de>
2024-12-08 16:09:35 +0100

    Fix back for options menu

--- ui_scenes/main_menu/credits/Credits.gd
+++ ui_scenes/main_menu/credits/Credits.gd
@@ -1,18 +1,18 @@
 extends Control
 
 
-func _on_back_pressed() -> void:
-	_on_button_click()
-	get_tree().change_scene_to_file("res://ui_scenes/main_menu/main_menu.tscn")
-
-
-func _on_back_mouse_entered() -> void:
-	_on_button_hover()
-
-
 func _on_button_hover() -> void:
 	Audio.play("button_hover")
 
 
 func _on_button_click() -> void:
 	Audio.play("button_select")
+
+
+func _on_back_options_pressed() -> void:
+	_on_button_click()
+	get_tree().change_scene_to_file("res://ui_scenes/main_menu/main_menu.tscn")
+
+
+func _on_back_options_mouse_entered() -> void:
+	_on_button_hover()
